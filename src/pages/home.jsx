b2d28a36bdb349e13f6209961dd45fb2ba738347
carlos12huet <carlos777daniel@live.com>
2024-10-24 16:33:42 -0600

    Firstcommit

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import { NavBar } from "./includes/navBar"
+import { LeftBar } from "./includes/leftBar"
+
+export const Home = () => {
+    return (
+        <>
+            <NavBar/>
+            <LeftBar/>
+
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+                    
+                </div>
+            </div>
+        </>
+    )
+}

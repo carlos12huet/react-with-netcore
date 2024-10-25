64b0cb8b103d76c3e3cfd4de3533281c20407c45
carlos12huet <carlos777daniel@live.com>
2024-10-25 15:05:50 -0600

    className

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
             <LeftBar/>
 
             <div className="p-4 sm:ml-64">
-                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg mt-14">
                     
                 </div>
             </div>
--- src/pages/includes/leftBar.jsx
+++ src/pages/includes/leftBar.jsx
@@ -5,25 +5,28 @@ export const LeftBar = () => {
     <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 " aria-label="Sidebar">
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white ">
             <ul className="space-y-2 font-medium">
-              
               <li>
-                <NavLink to="/">
-                  <div className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group">
-                    {/* <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                        <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-                    </svg> */}
-                    <span className="flex-1 ms-3 whitespace-nowrap">Inicio</span>
-                  </div>
+                <NavLink 
+                  to="/" 
+                  className={({ isActive }) => 
+                    isActive 
+                      ? "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group bg-gray-200" // Estilos activos
+                      : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group" // Estilos inactivos
+                  }
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/login"> 
-                  <div className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group">
-                    {/* <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                    </svg> */}
-                    <span className="flex-1 ms-3 whitespace-nowrap">Inicia sesion</span>
-                  </div>
+                <NavLink 
+                  to="/login" 
+                  className={({ isActive }) => 
+                    isActive 
+                      ? "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group bg-gray-200" // Estilos activos
+                      : "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group" // Estilos inactivos
+                  }
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">Inicia sesión</span>
                 </NavLink>
               </li>
               <li>
